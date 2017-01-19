95eaaf3d9fe4ce20b3df4469815a39c6a804c8f7
Rodrigues Lourenço Daniel <daniel.24.rodrigues@gmail.com>
2017-01-19 16:54:13 +0100

    Add class Player + Binding Play Time

--- OtHelloWorld/OtHelloWorld/Game.cs
+++ OtHelloWorld/OtHelloWorld/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,21 @@ namespace OtHelloWorld
         private int[,] board;
         private List<ImageBrush> pawnsColor;
         private bool isWhite;
+        private List<Player> players;
 
-        enum Colors { white = 0, black }
+        enum Colors { black = 0, white}
 
         public Game ()
         {
             board = new int[8, 8];
             pawnsColor = new List<ImageBrush>();
-            pawnsColor.Add(createBrushFromImage("whitePawn.png"));
             pawnsColor.Add(createBrushFromImage("blackPawn.png"));
+            pawnsColor.Add(createBrushFromImage("whitePawn.png"));
+            players = new List<Player>();
+            players.Add(new Player());
+            players.Add(new Player());
             isWhite = false;
+            players[(int)Colors.black].StartTimer();
         }
 
 
@@ -43,10 +49,36 @@ namespace OtHelloWorld
         {
             board[x,y] = isWhite ? (int)Colors.white : (int)Colors.black;
             isWhite = !isWhite;
+            if (isWhite)
+            {
+                players[(int)Colors.black].StopTimer();
+                //players[(int)Colors.black].Score = players[(int)Colors.black].Score + 1;
+                players[(int)Colors.white].StartTimer();
+
+            }
+            else
+            {
+                players[(int)Colors.white].StopTimer();
+                //players[(int)Colors.white].Score = players[(int)Colors.white].Score + 1;
+                players[(int)Colors.black].StartTimer();
+            }
         }
 
         public ImageBrush getBrush(int color) {
             return pawnsColor[color];
+        }
+
+        public List<Player> Players
+        {
+            get
+            {
+                return players;
+            }
+
+            set
+            {
+                players = value;
+            }
         }
 
     }
